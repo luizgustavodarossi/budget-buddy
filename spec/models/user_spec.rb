2935501f8833e0952115fc0bc9f4aa2228b1df2d
Luiz Gustavo R. Darossi <luizgustavodarossi@gmail.com>
2023-07-25 21:49:20 -0300

    Fix transaction association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'has many credit cards' do
-    user = User.reflect_on_association(:credit_cards)
-    expect(user.macro).to eq(:has_many)
+  describe 'authentication' do
+    it ''
   end
 
-  it 'has many accounts' do
-    user = User.reflect_on_association(:accounts)
-    expect(user.macro).to eq(:has_many)
-  end
+  describe 'associations' do
+    it 'has many credit cards' do
+      user = User.reflect_on_association(:credit_cards)
+      expect(user.macro).to eq(:has_many)
+    end
 
-  it 'has many categories' do
-    user = User.reflect_on_association(:categories)
-    expect(user.macro).to eq(:has_many)
-  end
+    it 'has many accounts' do
+      user = User.reflect_on_association(:accounts)
+      expect(user.macro).to eq(:has_many)
+    end
 
-  it 'has many transactions' do
-    user = User.reflect_on_association(:transactions)
-    expect(user.macro).to eq(:has_many)
+    it 'has many categories' do
+      user = User.reflect_on_association(:categories)
+      expect(user.macro).to eq(:has_many)
+    end
+
+    it 'has many transactions' do
+      user = User.reflect_on_association(:transactions)
+      expect(user.macro).to eq(:has_many)
+    end
   end
 end
