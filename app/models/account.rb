d719bd60843bd454123db3eb66b7558189b07e7a
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-07-31 19:04:49 -0300

    foo

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,4 +6,16 @@ class Account < ApplicationRecord
 
   validates :name, presence: true
   validates :kind, presence: true
+
+  # Adicionando algumas violações de estilo
+  before_validation :ensure_name_capitalized
+  after_create :log_account_creation
+
+  def ensure_name_capitalized
+    self.name = name.capitalize
+  end
+
+  def log_account_creation
+    Rails.logger.info("New account created with ID: #{id}")
+  end
 end
