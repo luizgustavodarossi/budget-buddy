e2ac3380b9ff1150d23b414d0a728ca06323e4f6
Luiz Gustavo Retka Darossi <luizgustavodarossi@gmail.com>
2023-08-03 07:20:12 -0300

    Category controller tests (#11)

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe AccountsController, type: :controller do
+RSpec.describe CategoriesController, type: :controller do
   describe "GET #index" do
     context "when user is logged in" do
       let(:current_user) { create :user }
-      let!(:accounts) { create_list :account, 3, user: current_user }
+      let!(:categories) { create_list :category, 3, user: current_user }
       before { sign_in current_user }
 
       it "returns http success" do
@@ -12,13 +12,13 @@ RSpec.describe AccountsController, type: :controller do
         expect(response).to have_http_status :success
       end
 
-      it "returns only the current user's accounts" do
+      it "returns only the current user's categories" do
         another_user = create :user
-        another_account = create :account, user: another_user
+        another_category = create :category, user: another_user
 
         get :index
-        expect(assigns(:accounts)).to match_array accounts
-        expect(assigns(:accounts)).not_to include another_account
+        expect(assigns(:categories)).to match_array categories
+        expect(assigns(:categories)).not_to include another_category
       end
 
       it "renders the index template" do
@@ -28,7 +28,7 @@ RSpec.describe AccountsController, type: :controller do
     end
 
     context "when user is not logged in" do
-      let!(:accounts) { create_list :account, 3 }
+      let!(:categories) { create_list :category, 3 }
 
       it "returns http redirect" do
         get :index
@@ -40,37 +40,37 @@ RSpec.describe AccountsController, type: :controller do
   describe "GET #show" do
     context "when user is logged in" do
       let(:current_user) { create :user }
-      let(:account) { create :account, user: current_user }
+      let(:category) { create :category, user: current_user }
       before { sign_in current_user }
 
       it "returns http success" do
-        get :show, params: { id: account.id }
+        get :show, params: { id: category.id }
         expect(response).to have_http_status :success
       end
 
-      it "returns only the current user's account" do
+      it "returns only the current user's category" do
         another_user = create :user
-        another_account = create :account, user: another_user
+        another_category = create :category, user: another_user
 
-        get :show, params: { id: account.id }
-        expect(assigns(:account)).to eq account
+        get :show, params: { id: category.id }
+        expect(assigns(:category)).to eq category
 
         expect do
-          get :show, params: { id: another_account.id }
+          get :show, params: { id: another_category.id }
         end.to raise_error(ActiveRecord::RecordNotFound)
       end
 
       it "renders the show template" do
-        get :show, params: { id: account.id }
+        get :show, params: { id: category.id }
         expect(response).to render_template :show
       end
     end
 
     context "when user is not logged in" do
-      let(:account) { create :account }
+      let(:category) { create :category }
 
       it "returns http redirect" do
-        get :show, params: { id: account.id }
+        get :show, params: { id: category.id }
         expect(response).to have_http_status(:redirect)
       end
     end
@@ -86,10 +86,10 @@ RSpec.describe AccountsController, type: :controller do
         expect(response).to have_http_status :success
       end
 
-      it "returns a new account created by the current user" do
+      it "returns a new category created by the current user" do
         get :new
-        expect(assigns(:account)).to be_a_new(Account)
-        expect(assigns(:account).user).to eq current_user
+        expect(assigns(:category)).to be_a_new(Category)
+        expect(assigns(:category).user).to eq current_user
       end
 
       it "renders the new template" do
@@ -109,37 +109,37 @@ RSpec.describe AccountsController, type: :controller do
   describe "GET #edit" do
     context "when user is logged in" do
       let(:current_user) { create :user }
-      let(:account) { create :account, user: current_user }
+      let(:category) { create :category, user: current_user }
       before { sign_in current_user }
 
       it "returns http success" do
-        get :edit, params: { id: account.id }
+        get :edit, params: { id: category.id }
         expect(response).to have_http_status :success
       end
 
-      it "returns only the current user's account" do
+      it "returns only the current user's category" do
         another_user = create :user
-        another_account = create :account, user: another_user
+        another_category = create :category, user: another_user
 
-        get :edit, params: { id: account.id }
-        expect(assigns(:account)).to eq account
+        get :edit, params: { id: category.id }
+        expect(assigns(:category)).to eq category
 
         expect do
-          get :edit, params: { id: another_account.id }
+          get :edit, params: { id: another_category.id }
         end.to raise_error(ActiveRecord::RecordNotFound)
       end
 
       it "renders the edit template" do
-        get :edit, params: { id: account.id }
+        get :edit, params: { id: category.id }
         expect(response).to render_template :edit
       end
     end
 
     context "when user is not logged in" do
-      let(:account) { create :account }
+      let(:category) { create :category }
 
       it "returns http redirect" do
-        get :edit, params: { id: account.id }
+        get :edit, params: { id: category.id }
         expect(response).to have_http_status(:redirect)
       end
     end
@@ -151,41 +151,41 @@ RSpec.describe AccountsController, type: :controller do
       before { sign_in current_user }
 
       context "with valid params" do
-        let(:valid_params) { attributes_for :account }
+        let(:valid_params) { attributes_for :category }
 
-        it "creates a new account for the current user" do
+        it "creates a new category for the current user" do
           expect do
-            post :create, params: { account: valid_params }
-          end.to change(current_user.accounts, :count).by(1)
+            post :create, params: { category: valid_params }
+          end.to change(current_user.categories, :count).by 1
         end
 
-        it "redirects to the created account" do
-          post :create, params: { account: valid_params }
-          expect(response).to redirect_to account_path(Account.last)
+        it "redirects to the created category" do
+          post :create, params: { category: valid_params }
+          expect(response).to redirect_to category_path(Category.last)
         end
       end
 
       context "with invalid params" do
-        let(:invalid_params) { attributes_for :account, name: nil }
+        let(:invalid_params) { attributes_for :category, name: nil }
 
-        it "does not create a new account" do
+        it "does not create a new category" do
           expect do
-            post :create, params: { account: invalid_params }
-          end.to change(Account, :count).by(0)
+            post :create, params: { category: invalid_params }
+          end.to change(Category, :count).by(0)
         end
 
         it "renders the new template" do
-          post :create, params: { account: invalid_params }
+          post :create, params: { category: invalid_params }
           expect(response).to render_template :new
         end
       end
     end
 
     context "when user is not logged in" do
-      let(:valid_params) { attributes_for :account }
+      let(:valid_params) { attributes_for :category }
 
       it "returns http redirect" do
-        post :create, params: { account: valid_params }
+        post :create, params: { category: valid_params }
         expect(response).to have_http_status(:redirect)
       end
     end
@@ -194,46 +194,46 @@ RSpec.describe AccountsController, type: :controller do
   describe "PATCH #update" do
     context "when user is logged in" do
       let(:current_user) { create :user }
-      let(:account) { create :account, user: current_user }
+      let(:category) { create :category, user: current_user }
       before { sign_in current_user }
 
       context "with valid params" do
-        let(:valid_params) { attributes_for :account, name: "Updated Name" }
+        let(:valid_params) { attributes_for :category, name: "Update Name" }
 
-        it "updates the account" do
-          patch :update, params: { id: account.id, account: valid_params }
-          account.reload
-          expect(account.name).to eq "Updated Name"
+        it "updates the category" do
+          patch :update, params: { id: category.id, category: valid_params }
+          category.reload
+          expect(category.name).to eq "Update Name"
         end
 
-        it "redirects to the updated account" do
-          patch :update, params: { id: account.id, account: valid_params }
-          expect(response).to redirect_to account_path(account)
+        it "redirects to the updated category" do
+          patch :update, params: { id: category.id, category: valid_params }
+          expect(response).to redirect_to category_path(category)
         end
       end
 
       context "with invalid params" do
-        let(:invalid_params) { attributes_for :account, name: nil }
+        let(:invalid_params) { attributes_for :category, name: nil }
 
-        it "does not update the account" do
-          patch :update, params: { id: account.id, account: invalid_params }
-          account.reload
-          expect(account.name).not_to be_nil
+        it "does not update the category" do
+          patch :update, params: { id: category.id, category: invalid_params }
+          category.reload
+          expect(category.name).not_to be_nil
         end
 
         it "renders the edit template" do
-          patch :update, params: { id: account.id, account: invalid_params }
+          patch :update, params: { id: category.id, category: invalid_params }
           expect(response).to render_template :edit
         end
       end
     end
 
     context "when user is not logged in" do
-      let(:account) { create :account }
-      let(:valid_params) { attributes_for :account, name: "Updated Name" }
+      let(:category) { create :category }
+      let(:valid_params) { attributes_for :category, name: "Update Name" }
 
       it "returns http redirect" do
-        patch :update, params: { id: account.id, account: valid_params }
+        patch :update, params: { id: 1, category: valid_params }
         expect(response).to have_http_status(:redirect)
       end
     end
@@ -242,26 +242,26 @@ RSpec.describe AccountsController, type: :controller do
   describe "DELETE #destroy" do
     context "when user is logged in" do
       let(:current_user) { create :user }
-      let!(:account) { create :account, user: current_user }
+      let!(:category) { create :category, user: current_user }
       before { sign_in current_user }
 
-      it "destroys the account" do
+      it "destroys the category" do
         expect do
-          delete :destroy, params: { id: account.id }
-        end.to change(Account, :count).by(-1)
+          delete :destroy, params: { id: category.id }
+        end.to change(Category, :count).by(-1)
       end
 
-      it "redirects to the accounts index" do
-        delete :destroy, params: { id: account.id }
-        expect(response).to redirect_to accounts_path
+      it "redirects to the categories index" do
+        delete :destroy, params: { id: category.id }
+        expect(response).to redirect_to categories_path
       end
     end
 
     context "when user is not logged in" do
-      let!(:account) { create :account }
+      let!(:category) { create :category }
 
       it "returns http redirect" do
-        delete :destroy, params: { id: account.id }
+        delete :destroy, params: { id: category.id }
         expect(response).to have_http_status(:redirect)
       end
     end
