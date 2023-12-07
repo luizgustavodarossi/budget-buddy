9d908ec105ffb991bd46bec06b2929448adcee62
Luiz Gustavo R. Darossi <luizgustavodarossi@gmail.com>
2023-12-07 12:38:06 -0300

    Layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :template_for_devise, if: :devise_controller?
+
+  private
+
+  def template_for_devise
+    devise_controller? ? 'devise' : 'application'
+  end
 end
